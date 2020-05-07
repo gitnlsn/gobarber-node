00bd66339f3bd27190305fadb500c6af91d08efd
nelson kenzo tamashiro <nelsonkenzotamashiro@gmail.com>
2020-05-07 01:37:45 -0300

    Configured jest coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,15 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+
+    collectCoverage: true,
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.ts',
+        '!<rootDir>/src/database/migrations/*.ts',
+        '!<rootDir>/src/**/index.ts',
+    ],
+    coverageReporters: [
+        'text-summary',
+        'lcov',
+    ],
 };
